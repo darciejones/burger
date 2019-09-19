5d17414b8a9d602284e5113dde21022d8d8fe7a0
darciejones <47013418+darciejones@users.noreply.github.com>
2019-09-18 22:57:47 -0400

    Add files via upload
    just started. some files created.

---
+++ controllers/routes.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+
+router.get("/", function(req, res) {
+    res.render('index');
+
+})
+
+
+
+
